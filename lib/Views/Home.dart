bebffaac7570014169c06ec20cc2b6aa8bf74de3
Zawiyar Khan <zawiyarkhan007@gmail.com>
2024-01-27 01:56:00 +0500

    commit

--- lib/Views/Home.dart
+++ lib/Views/Home.dart
@@ -3,7 +3,6 @@ import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:kalorist_mvp/consts.dart';
 
 class Home extends StatefulWidget {
   String id;
